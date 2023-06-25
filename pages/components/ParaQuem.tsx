b1ac0d5c42e2a78b52a3b26f6ad0235ffc46e85b
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-06-25 16:52:43 -0300

    new link

--- pages/components/ParaQuem.tsx
+++ pages/components/ParaQuem.tsx
@@ -34,7 +34,7 @@ const ParaQuem = (props: Props) => {
           </p>
           <div className="flex lg:justify-end justify-center md:p-20">
             <a
-              href="https://pay.hotmart.com/A69854210Q"
+              href="https://go.hotmart.com/P84169592I?dp=1"
               id="kanit"
               className="rounded-3xl md:px-14 px-10 py-5 bg-gradient-to-r from-[#c368d1] to-[#ff00e5] md:text-4xl text-3xl font-semibold md:mt-3 mt-14 text-center hover:brightness-75"
             >
