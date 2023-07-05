df9a4b08875ec963be52cb3dd343243a84879c81
markoliv <marcosgarcia.oliveira02@gmail.com>
2023-07-05 12:58:36 -0300

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,8 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <head>
-        <meta
-          name="facebook-domain-verification"
-          content="7np134rsdkaz2j7dmjcwu2twcep71r"
-        />
-      </head>
+    <>
       <Component {...pageProps} />
-    </div>
+    </>
   );
 }
